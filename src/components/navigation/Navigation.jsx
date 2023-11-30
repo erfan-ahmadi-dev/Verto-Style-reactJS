9f584d6ec57d66cc8fbe7196326e6e54e795f002
erfan ahmadi <dornadev@gmail.com>
2023-11-30 22:06:23 +0330

    Implement header design and navigation

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,7 +1,52 @@
 import React from "react";
-
+import { NavLink } from "react-router-dom";
+import { Texts } from "../../utils/Constants";
+import { PATHS } from "../../configs/RoutesConfig";
 function Navigation() {
-  return <div></div>;
+  return (
+    <nav
+      className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
+      id="mobile-menu-2 "
+    >
+      <ul className="flex flex-row w-full gap-6 md:gap-4 text-colorPrimary cursor-pointer hideOnSmall font-[IranRegular] text-sm ">
+        <li>
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? "activeMenu" : "")}
+          >
+            {Texts.home}
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to={PATHS.PRODUCTS}
+            className={({ isActive }) => (isActive ? "activeMenu" : "")}
+          >
+            {Texts.products}
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to={PATHS.PAGE404}
+            className={({ isActive }) => (isActive ? "activeMenu" : "")}
+          >
+            {Texts.blog}
+          </NavLink>
+          {/* <Link to="/blog">{persianText.blog}</Link> */}
+        </li>
+        <li>
+          <NavLink
+            // TODO change Link to
+            to={PATHS.REGISTER}
+            className={({ isActive }) => (isActive ? "activeMenu" : "")}
+          >
+            {Texts.contactus}
+          </NavLink>
+          {/* <Link to="/contact-us">{persianText.contactus}</Link> */}
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Navigation;
--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -1,7 +1,45 @@
 import React from "react";
-
+import logo from "../../assets/images/logoblack.svg";
+import { Link } from "react-router-dom";
+import { PATHS } from "../../configs/RoutesConfig";
+import {
+  MdMenu as MenuIcon,
+  MdOutlineShoppingCart as CartIcon,
+  MdPersonOutline as AccountIcon,
+} from "react-icons/md";
+import Navigation from "../../components/navigation/Navigation";
+// TODO Add drawer
 function Header() {
-  return <div></div>;
+  return (
+    <header className="shadow-md ">
+      <nav className="flex bg-white border-gray-200 px-5  py-4 dark:bg-gray-800">
+        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl w-full self-start">
+          <Link to="/" className="flex items-center">
+            <img src={logo} className="mr-3 h-6 sm:h-7" alt="vertoStyle Logo" />
+          </Link>
+          <div className="flex items-center justify-between lg:order-2 gap-4 ">
+            <Link to={PATHS.LOGIN} className="hideOnSmall ">
+              <AccountIcon className="h-6 w-6" />
+            </Link>
+            <Link to={PATHS.BASKET} className="hideOnSmall ">
+              <CartIcon className="h-6 w-6" />
+            </Link>
+
+            <button
+              type="button"
+              className="items-center ml-1 rounded-lg lg:hidden "
+              data-drawer-target="drawer-navigation"
+              data-drawer-show="drawer-navigation"
+              aria-controls="drawer-navigation"
+            >
+              <MenuIcon className="h-6 w-6" />
+            </button>
+          </div>
+          <Navigation />
+        </div>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
