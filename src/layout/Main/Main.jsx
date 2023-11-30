edb10ce136377dbd0b6a8f2ff2fe2dc1ab5ad340
erfan ahmadi <dornadev@gmail.com>
2023-11-30 20:43:45 +0330

    Implement Main Layout

--- src/layout/Main/Main.jsx
+++ src/layout/Main/Main.jsx
@@ -1,7 +1,15 @@
 import React from "react";
-
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import { Outlet } from "react-router-dom";
 function Main() {
-  return <div></div>;
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
 }
 
 export default Main;
