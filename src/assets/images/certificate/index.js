30f1cd09e7f3826f91ba8458d93eb21130058cb0
erfan ahmadi <dornadev@gmail.com>
2023-12-12 17:54:07 +0330

    feat: add certificate imaegs to assets

---
+++ src/assets/images/certificate/index.js
@@ -0,0 +1,5 @@
+import enmad from "../certificate/enamad.png";
+import etehadye from "../certificate/etehadye.png";
+import samandehi from "../certificate/samandehi.png";
+
+export { enmad, etehadye, samandehi };
--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -3,5 +3,6 @@ import slide2 from "../images/slide2.jpg";
 import slide3 from "../images/slide3.jpg";
 import * as banner from "./banners/index";
 import * as category from "./categoris/index";
-export { slide1, slide2, slide3, banner, category };
+import * as certicates from "./certificate/index";
+export { slide1, slide2, slide3, banner, category, certicates };
 // export * from "./categoris/index";
