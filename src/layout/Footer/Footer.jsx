009ce05c4e36655851bee67fb36bdeb6da8860e8
erfan ahmadi <dornadev@gmail.com>
2023-12-12 17:58:49 +0330

    style: add font to footer text

--- src/layout/Footer/Footer.jsx
+++ src/layout/Footer/Footer.jsx
@@ -5,14 +5,15 @@ import faTexts from "../../utils/Constants";
 import { certicates } from "../../assets/images";
 function Footer() {
   return (
-    <footer className="p-4 bg-white md:p-8 lg:p-10 dark:bg-gray-800  mt-5 ">
+    <footer className="p-4 bg-white md:p-8 lg:p-10 dark:bg-gray-800  mt-5  ">
       <div className="flex flex-col mx-auto max-w-screen-xl text-center justify-center items-center border-t-2 border-gray-200 py-4">
         <div className="w-full items-center flex justify-center">
           <img src={logo} className="h-10" alt="" />
         </div>
         <div className=" w-full  flex justify-between align-middle items-center gap-4 border-b-2 flex-col lg:flex-row py-2">
-          <p className=" w-full lg:w-9/12 text-justify my-6 px-4 text-gray-500 dark:text-gray-400 leading-8">
-            <span className="font-bold">{faTexts.storeName}</span> <br />
+          <p className=" w-full lg:w-9/12 text-justify my-6 px-4 text-gray-500 dark:text-gray-400 leading-8 font-IranRegular">
+            <span className="font-IranExtraBold ">{faTexts.storeName}</span>
+            <br />
             {faTexts.footerText}
           </p>
           <div className="flex gap-2   ">
