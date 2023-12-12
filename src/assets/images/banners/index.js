05cd6e7fcd2179df90e47436a08b28328500e861
erfan ahmadi <dornadev@gmail.com>
2023-12-12 17:13:26 +0330

    feat: add banner images and index

---
+++ src/assets/images/banners/index.js
@@ -0,0 +1,15 @@
+import menClotheBanner from "../banners/mens.jpg";
+import menShoesBanner from "../banners/menShoes.jpg";
+import menSportBanner from "../banners/menSport.jpg";
+import womenClotheBanner from "../banners/women.jpg";
+import womenShoesBanner from "../banners/womenShoes.jpg";
+import womenSportBanner from "../banners/womenSport.jpg";
+
+export {
+  menClotheBanner,
+  menShoesBanner,
+  menSportBanner,
+  womenClotheBanner,
+  womenShoesBanner,
+  womenSportBanner,
+};
