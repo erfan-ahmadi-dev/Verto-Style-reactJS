924e1c937f04d64f86097c391bf801ec19a3f7f0
erfan ahmadi <dornadev@gmail.com>
2023-12-14 10:40:19 +0330

    feat: add live data fetching and skeleton

--- src/pages/product/SingleProduct.jsx
+++ src/pages/product/SingleProduct.jsx
@@ -2,77 +2,41 @@ import { BsShieldCheck as WarrantyIcon } from "react-icons/bs";
 import { LiaShippingFastSolid as ShippingIcon } from "react-icons/lia";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
-// TODO disable button when is loading
-function SingleProduct(isLoading) {
-  isLoading = true;
-  const data = {
-    rating: {
-      rate: 0,
-      count: 0,
-    },
-    _id: "6569d16f7d8862dfba146f4b",
-    category: "6561eaa51adeb81260019942",
-    subcategory: "6561eb0c1adeb81260019951",
-    name: "تی شرت تست",
-    price: 650000,
-    quantity: 8,
-    brand: "باینت",
-    description: "",
-    thumbnail: "products-6569d16f7d8862dfba146f4b-1701433711840.jpeg",
-    images: [
-      "products-6569d16f7d8862dfba146f4b-1701433711894-1.jpeg",
-      "products-6569d16f7d8862dfba146f4b-1701433711894-2.jpeg",
-      "products-6569d16f7d8862dfba146f4b-1701433711895-3.jpeg",
-    ],
-    createdAt: "2023-12-01T12:28:31.811Z",
-    updatedAt: "2023-12-01T12:28:31.976Z",
-    slugname: "ty-shrt-aastyn-blnd-mrdanh-baynt-mdl-2261571-571-rng-mshky",
+import { Link, useParams } from "react-router-dom";
+import { getData } from "../../api/defaultApi";
+import { useQuery } from "@tanstack/react-query";
+import { IoIosArrowBack as ArrowIcon } from "react-icons/io";
+// TODO disable button when is loading / add dynamic size
+function SingleProduct() {
+  const params = useParams();
+  let data;
+  const fetchProduct = () => {
+    return getData(`products/${params.productId}`);
   };
+  const query = useQuery({
+    queryKey: ["productWithId"],
+    queryFn: fetchProduct,
+  });
+  if (!query.isLoading) {
+    data = query.data.data.product;
+  }
 
-  const dataHandler = (element, sklCount = 1, sklwidth, sklheight) => {
-    return isLoading ? (
-      <Skeleton count={sklCount} width={sklwidth} height={sklheight} />
-    ) : (
-      element
-    );
-  };
   return (
     <div className=" dark:bg-gray-800 py-8">
       <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
-        {/* breadcrumb */}
         <nav className="flex mb-4 " aria-label="Breadcrumb">
           <ol className="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
             <li className="inline-flex items-center">
-              <a
-                href="/"
-                className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white"
-              >
-                {data.subcategory}
-              </a>
+              <Link to="/" className="breadcumpText">
+                {query.isLoading ? <Skeleton /> : data.category.name}
+              </Link>
             </li>
             <li>
               <div className="flex items-center">
-                <svg
-                  className="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 6 10"
-                >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="m1 9 4-4-4-4"
-                  />
-                </svg>
-                <a
-                  href="/"
-                  className="ms-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ms-2 dark:text-gray-400 dark:hover:text-white"
-                >
-                  {data.subCategory}
-                </a>
+                <ArrowIcon className="ml-2" />
+                <Link to="/" className="breadcumpText">
+                  {query.isLoading ? <Skeleton /> : data.subcategory.name}
+                </Link>
               </div>
             </li>
           </ol>
@@ -80,7 +44,7 @@ function SingleProduct(isLoading) {
         <div className="flex flex-col md:flex-row -mx-4 border-b-2 py-4">
           <div className="md:flex-1 px-4">
             <div className="h-fit rounded-lg bg-gray-300 dark:bg-gray-700 mb-4">
-              {isLoading ? (
+              {query.isLoading ? (
                 <Skeleton height={500} />
               ) : (
                 <img
@@ -95,34 +59,34 @@ function SingleProduct(isLoading) {
           <div className="md:flex-1 p-4 gap-4 flex-col flex justify-between">
             <div className="flex flex-col gap-5">
               <h2 className="text-xl  text-gray-800 dark:text-white mb-2">
-                {dataHandler(data.name)}
+                {query.isLoading ? <Skeleton /> : data.name}
               </h2>
               <h3 className=" text-base text-gray-500 dark:text-white">
-                {dataHandler(data.brand)}
+                {query.isLoading ? <Skeleton /> : data.brand}
               </h3>
             </div>
 
             <div className="flex flex-col h-fit gap-4">
-              {dataHandler(
+              {query.isLoading ? (
+                <Skeleton width={200} />
+              ) : (
                 <div className="flex gap-2">
                   <ShippingIcon className="h-full w-5 " />
                   <span className="text-sm">آماده ارسال</span>
-                </div>,
-                1,
-                200
+                </div>
               )}
-              {dataHandler(
+              {query.isLoading ? (
+                <Skeleton width={200} />
+              ) : (
                 <div className="flex gap-2">
                   <WarrantyIcon className="h-full w-5" />
                   <span className="text-sm">گارانتی اصالت و سلامت فیزیکی</span>
-                </div>,
-                1,
-                200
+                </div>
               )}
             </div>
             <div className="mb-4">
               <span className="font-bold text-gray-700 dark:text-gray-300">
-                {dataHandler("سایز", 1, 70)}
+                {query.isLoading ? <Skeleton width={70} /> : "سایز"}
               </span>
               <div className="flex items-center mt-2">
                 <ul className="flex w-full gap-6 ">
@@ -135,7 +99,7 @@ function SingleProduct(isLoading) {
                       className="hidden peer"
                     />
                     <label
-                      for="hosting-big"
+                      htmlFor="hosting-big"
                       className="inline-flex items-center justify-between w-fit p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer  peer-checked:border-red-600 peer-checked:text-red-600  hover:bg-gray-100"
                     >
                       <div>
@@ -152,7 +116,7 @@ function SingleProduct(isLoading) {
                       className="hidden peer"
                     />
                     <label
-                      for="large"
+                      htmlFor="large"
                       className="inline-flex items-center justify-between w-fit p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer  peer-checked:border-red-600 peer-checked:text-red-600  hover:bg-gray-100"
                     >
                       <div className="w-fit text-lg font-semibold">L</div>
@@ -167,7 +131,7 @@ function SingleProduct(isLoading) {
                       className="hidden peer"
                     />
                     <label
-                      for="xl"
+                      htmlFor="xl"
                       className="inline-flex items-center justify-between w-fit p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer  peer-checked:border-red-600 peer-checked:text-red-600  hover:bg-gray-100"
                     >
                       <div>
@@ -181,11 +145,11 @@ function SingleProduct(isLoading) {
 
             <div className="flex   py-4   items-center justify-between">
               <div className="w-2/5 px-2">
-                <button className="primaryButton">افزودن به سبد</button>
+                <button className="outlineButton">افزودن به سبد</button>
               </div>
               <div className="mr-4 ">
                 <span className="text-gray-600 dark:text-gray-300 w-2/5">
-                  {isLoading ? (
+                  {query.isLoading ? (
                     <Skeleton width={100} />
                   ) : (
                     data.price.toLocaleString("fa-IR").concat(" تومان")
@@ -199,13 +163,17 @@ function SingleProduct(isLoading) {
           <div className="flex flex-col gap-4 w-3/12">
             <h3 className="font-bold">ویژگی های </h3>
             <h4 className="text-xs">
-              {isLoading ? <Skeleton width={200} /> : data.name}
+              {query.isLoading ? <Skeleton width={200} /> : data.name}
             </h4>
           </div>
           <div className="flex flex-col gap-4">
             <h3 className="font-bold">مشخصات </h3>
             <div className=" text-gray-700 dark:text-gray-300 whitespace-pre leading-8 text-sm">
-              {data.description || <Skeleton count={8} width={200} />}
+              {query.isLoading ? (
+                <Skeleton width={200} count={8} />
+              ) : (
+                data.description
+              )}
             </div>
           </div>
         </div>
