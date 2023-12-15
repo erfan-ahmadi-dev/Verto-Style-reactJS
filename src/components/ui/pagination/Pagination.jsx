05b10dc36e93ce4740c5f11d3594d5d41312cfb1
erfan ahmadi <dornadev@gmail.com>
2023-12-15 17:33:10 +0330

    fix: max count of pagination item

--- src/components/ui/pagination/Pagination.jsx
+++ src/components/ui/pagination/Pagination.jsx
@@ -1,16 +1,46 @@
 import React from "react";
 import { MdNavigateNext, MdNavigateBefore } from "react-icons/md";
+
 function Pagination({ data, setPage, page }) {
+  console.log(data.total_pages);
   const handleNext = () => {
     if (page !== data.total_pages) {
       setPage(page + 1);
     }
   };
+
   const handlePrevious = () => {
     if (page !== 1) {
       setPage(page - 1);
     }
   };
+
+  const totalPages = data.total_pages;
+
+  const maxPagesToShow = 10;
+
+  // Calculate the range of pages to display
+  let startPage = Math.max(
+    1,
+    Math.min(
+      page - Math.floor(maxPagesToShow / 2),
+      totalPages - maxPagesToShow + 1
+    )
+  );
+
+  let endPage = Math.min(totalPages, startPage + maxPagesToShow - 1);
+
+  const pageArrayLength = Math.max(endPage - startPage + 1, 0); // Ensure non-negative length
+  console.log(
+    "total ",
+    data.total_pages,
+    "start",
+    startPage,
+    " end",
+    endPage,
+    " length ",
+    pageArrayLength
+  );
   return (
     <nav aria-label="Page navigation example" className="py-5">
       <ul className="flex items-center -space-x-px h-8 text-sm">
@@ -22,17 +52,30 @@ function Pagination({ data, setPage, page }) {
             <MdNavigateNext />
           </span>
         </li>
-        {[...Array(data.total_pages)].map((item, index) => (
-          <li key={index} className="cursor-pointer">
-            <span
-              onClick={() => setPage(index + 1)}
-              className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-            >
-              {index + 1}
-            </span>
-          </li>
-        ))}
+        {Array.from({ length: pageArrayLength }).map((_, index) => {
+          const pageNumber = startPage + index;
 
+          // Add your conditions for rendering the <li> element
+          const isCurrentPage = pageNumber === page;
+
+          return (
+            <li
+              key={index}
+              className={`cursor-pointer ${
+                isCurrentPage ? "current-page" : ""
+              }`}
+            >
+              <span
+                onClick={() => setPage(pageNumber)}
+                className={`flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white ${
+                  isCurrentPage ? "current-page-span" : ""
+                }`}
+              >
+                {pageNumber}
+              </span>
+            </li>
+          );
+        })}
         <li>
           <span
             onClick={handleNext}
