94f6be58077479f34d3e7126a4f74459bcd5a50d
erfan ahmadi <dornadev@gmail.com>
2023-12-15 15:57:29 +0330

    feat: add toastify container

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { RouterProvider } from "react-router-dom";
 import routes from "./Route/Routes";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: { staleTime: 60 * 1000 },
@@ -11,6 +13,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <div className=" font-[IranRegular] scroll-smooth w-full overflow-hidden ">
+        <ToastContainer />
         <RouterProvider router={routes} />
         <ReactQueryDevtools initialIsOpen={false} />
       </div>
