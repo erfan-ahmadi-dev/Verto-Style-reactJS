96ffa7689ad8edf157664d51d844662f685b3227
erfan ahmadi <dornadev@gmail.com>
2023-12-15 16:37:10 +0330

    style: add cursour pointer

--- src/components/ui/pagination/Pagination.jsx
+++ src/components/ui/pagination/Pagination.jsx
@@ -17,13 +17,13 @@ function Pagination({ data, setPage, page }) {
         <li>
           <span
             onClick={handlePrevious}
-            className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+            className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white cursor-pointer"
           >
             <MdNavigateNext />
           </span>
         </li>
         {[...Array(data.total_pages)].map((item, index) => (
-          <li key={index}>
+          <li key={index} className="cursor-pointer">
             <span
               onClick={() => setPage(index + 1)}
               className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
@@ -36,7 +36,7 @@ function Pagination({ data, setPage, page }) {
         <li>
           <span
             onClick={handleNext}
-            className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+            className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white cursor-pointer"
           >
             <MdNavigateBefore />
           </span>
