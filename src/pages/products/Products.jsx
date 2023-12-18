f64bc24a2de4e5340a41fd1636ff57f80f2e78d0
erfan ahmadi <dornadev@gmail.com>
2023-12-18 16:38:38 +0330

    feat: add side bar of categories

--- src/pages/products/Products.jsx
+++ src/pages/products/Products.jsx
@@ -1,7 +1,81 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import { getData } from "../../api/defaultApi";
+import { useQuery } from "@tanstack/react-query";
+import LodingModal from "../../components/ui/modal/LoadingModal";
+import { NavLink, useParams } from "react-router-dom";
+import { PATHS } from "../../configs/RoutesConfig";
+import Card from "../../components/ui/card/Card";
+import Pagination from "../../components/ui/pagination/Pagination";
 function Products() {
-  return <div></div>;
+  let params = useParams();
+  const [page, setPage] = useState(1);
+  const [sortType, setSort] = useState("createdAt");
+  useEffect(() => {
+    setPage(1);
+  }, [params]);
+  const handleSelectChange = (event) => {
+    setSort(event.target.value);
+  };
+  const fetchCategories = () => {
+    const response = getData("categories");
+    return response;
+  };
+  const queryCategory = useQuery({
+    queryKey: ["categories"],
+    queryFn: fetchCategories,
+  });
+  const fetchSubCategories = () => {
+    const response = getData("subcategories");
+    return response;
+  };
+  const querySubCategory = useQuery({
+    queryKey: ["subcategories"],
+    queryFn: fetchSubCategories,
+  });
+  const fetchProducts = () => {
+    const response = getData(
+      `products?limit=5&page=${page}&subcategory=${params.category}&sort=${sortType}`
+    );
+    return response;
+  };
+  const queryProducts = useQuery({
+    queryKey: ["products", params, page, sortType],
+    queryFn: fetchProducts,
+  });
+  if (
+    queryCategory.isLoading ||
+    querySubCategory.isLoading ||
+    queryProducts.isLoading
+  ) {
+    return <LodingModal />;
+  }
+  if (queryCategory.data && querySubCategory.data) {
+    console.log("ok");
+  }
+
+  return (
+    <div className="flex gap-4 p-10 animate-fade font-IranRegular">
+      <aside className="w-2/6 h-fit p-5 rounded-2xl shadow-lg">
+        {queryCategory.data.data.categories.map((category, index) => (
+          <ul key={category._id} className="flex flex-col gap-2">
+            <li className="cursor-pointer">{category.name}</li>
+            {querySubCategory.data.data.subcategories
+              .filter((subcat) => subcat.category === category._id)
+              .map((subitem) => (
+                <li key={subitem._id} className="pr-4">
+                  <NavLink
+                    to={`${PATHS.PRODUCTS}/${subitem._id}`}
+                    className={({ isActive }) => (isActive ? "activeMenu" : "")}
+                  >
+                    {subitem.name}
+                  </NavLink>
+                </li>
+              ))}
+          </ul>
+        ))}
+      </aside>
+    </div>
+  );
 }
 
 export default Products;
