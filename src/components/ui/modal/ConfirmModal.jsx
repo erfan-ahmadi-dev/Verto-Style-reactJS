aed5d011a8720cd003b4e892bb84519dca7be75c
erfan ahmadi <dornadev@gmail.com>
2023-12-20 11:28:30 +0330

    refactor: change state managment

--- src/components/ui/modal/ConfirmModal.jsx
+++ src/components/ui/modal/ConfirmModal.jsx
@@ -3,30 +3,27 @@ import { useState } from "react";
 import { HiOutlineExclamationCircle } from "react-icons/hi";
 import faTexts from "../../../utils/Constants";
 
-function ConfirmModal({ isOpen }) {
-  const [openModal, setOpenModal] = useState(isOpen);
-
+function ConfirmModal({ isOpen, setOpenConfirm }) {
   return (
     <>
-      <Button onClick={() => setOpenModal(true)}>Toggle modal</Button>
       <Modal
-        show={openModal}
+        show={isOpen}
         size="md"
-        onClose={() => setOpenModal(false)}
+        onClose={() => setOpenConfirm(false)}
         popup
       >
         <Modal.Header />
         <Modal.Body>
-          <div className="text-center">
+          <div className="text-center ">
             <HiOutlineExclamationCircle className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200" />
             <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
               {faTexts.sureToDelete}
             </h3>
             <div className="flex justify-center gap-4">
-              <Button color="failure" onClick={() => setOpenModal(false)}>
+              <Button color="failure" onClick={() => setOpenConfirm(false)}>
                 {faTexts.yes}
               </Button>
-              <Button color="gray" onClick={() => setOpenModal(false)}>
+              <Button color="gray" onClick={() => setOpenConfirm(false)}>
                 {faTexts.no}
               </Button>
             </div>
