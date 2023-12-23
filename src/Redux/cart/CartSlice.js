16a17fda8cd65bf609fea8f08c8b4b65ae3db0d0
erfan ahmadi <dornadev@gmail.com>
2023-12-23 17:38:32 +0330

    feat: add increament and decreament count

--- src/Redux/cart/CartSlice.js
+++ src/Redux/cart/CartSlice.js
@@ -22,9 +22,28 @@ export const cartSlice = createSlice({
     deleteItem: (state, action) => {
       state.cart = state.cart.filter((item) => item.id !== action.payload);
     },
+    increaseCount: (state, action) => {
+      const existingItemIndex = state.cart.findIndex(
+        (item) => item.id === action.payload
+      );
+      if (existingItemIndex !== -1) {
+        state.cart[existingItemIndex].count =
+          state.cart[existingItemIndex].count + 1;
+      }
+    },
+    decreaseCount: (state, action) => {
+      const existingItemIndex = state.cart.findIndex(
+        (item) => item.id === action.payload
+      );
+      if (existingItemIndex !== -1) {
+        state.cart[existingItemIndex].count =
+          state.cart[existingItemIndex].count - 1;
+      }
+    },
   },
 });
 
-export const { addToCart, deleteItem } = cartSlice.actions;
+export const { addToCart, deleteItem, decreaseCount, increaseCount } =
+  cartSlice.actions;
 
 export default cartSlice.reducer;
