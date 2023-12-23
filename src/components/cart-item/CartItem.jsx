6c56b4af224a174bbf864352e56e546dd1d6c210
erfan ahmadi <dornadev@gmail.com>
2023-12-23 16:26:30 +0330

    refactor: removed unnecessary elemnts

--- src/components/cart-item/CartItem.jsx
+++ src/components/cart-item/CartItem.jsx
@@ -1,29 +1,28 @@
-import React from "react";
-import { FaPlus, FaMinus } from "react-icons/fa";
-import { MdDeleteForever } from "react-icons/md";
+import React, { useState } from "react";
+
 import { BASE_THUMBNAIL_URL } from "../../utils/Constants";
 import QuantityInput from "../ui/input/QuantityInput";
 function CartItem({ data }) {
+  const [quantity, setQuantity] = useState(data.count);
   // TODO add alt to image and data
   return (
     <div className="flex items-center gap-4 border-b-2 w-full h-fit justify-between px-4 font-IranRegular ">
       <img
-        src={`${BASE_THUMBNAIL_URL}/products-6583d45bfd72bb1bc6e1bbe1-1703138395969.jpeg`}
+        src={`${BASE_THUMBNAIL_URL}/${data.thumbnail}`}
         className="rounded-t-lg aspect-square w-32 h-fit p-2"
-        alt=""
+        alt="product thumbnail"
       />
-      <span className=" text-sm line-clamp-2 w-3/12">'تی شرت فلان'</span>
-      {/* <div className="flex gap-2 items-center bg-backColor w-fit text-menuTextColor px-4 focus:border-none focus:outline-none border-none outline-none focus:ring-transparent rounded-lg text-center ">
-        <FaPlus className="w-4 h-4 p-1 cursor-pointer" />
-        <input
-          type="text"
-          className=" bg-backColor w-8 text-menuTextColor p-2 focus:border-none focus:outline-none border-none outline-none focus:ring-transparent rounded-lg text-center"
-          value={1}
-        />
-        <FaMinus className="w-4 h-4 p-1 cursor-pointer" />
-      </div> */}
-      <QuantityInput className="w-10" />
-      <span className="w-3/12 text-center line-clamp-1">130,000 تومان</span>
+      <span className=" text-sm line-clamp-2 w-3/12">{data.name}</span>
+
+      <QuantityInput
+        className="w-10"
+        setQuantity={setQuantity}
+        quantity={quantity}
+        stock={data.stock}
+      />
+      <span className="w-3/12 text-center line-clamp-1">
+        {data.price.toLocaleString("fa-IR")} تومان
+      </span>
       <span className="deleteSpan ">حذف</span>
     </div>
   );
