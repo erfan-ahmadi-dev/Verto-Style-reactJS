96655b4826cd9055ad5ab5046b7e137237090369
erfan ahmadi <dornadev@gmail.com>
2023-12-23 13:23:51 +0330

    feat: add cart badge

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -8,13 +8,23 @@ import {
   MdPersonOutline as AccountIcon,
 } from "react-icons/md";
 import Navigation from "../../components/navigation/Navigation";
+import { useDispatch, useSelector } from "react-redux";
+import { useState } from "react";
 // TODO Add drawer
 function Header() {
   const localData = localStorage.getItem("accessToken");
-
+  const cartState = useSelector((state) => state.cart);
+  const [badge, setBadge] = useState(false);
+  useEffect(() => {
+    if (cartState.cart.length > 0) {
+      setBadge(true);
+    } else {
+      setBadge(false);
+    }
+  }, [cartState]);
   return (
-    <header className="shadow-md ">
-      <nav className="flex bg-white border-gray-200 px-5  py-4 dark:bg-gray-800">
+    <header className="shadow-md px-2 py-2 font-IranRegular">
+      <nav className="flex bg-white border-gray-200  dark:bg-gray-800 h-fit py-5">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl w-full self-start">
           <Link to="/" className="flex items-center">
             <img src={logo} className="mr-3 h-6 sm:h-7" alt="vertoStyle Logo" />
@@ -26,7 +36,16 @@ function Header() {
             >
               <AccountIcon className="h-6 w-6" />
             </Link>
-            <Link to={PATHS.BASKET} className="hideOnSmall ">
+            <Link to={PATHS.BASKET} className="hideOnSmall relative ">
+              {badge ? (
+                <div className="absolute animate-jump-in animate-ease-out -top-2 -right-2 bg-red-500 w-5 h-5 rounded-full flex justify-center items-center">
+                  <span className="text-white text-[10px]">
+                    {cartState.cart.length}
+                  </span>
+                </div>
+              ) : (
+                ""
+              )}
               <CartIcon className="h-6 w-6" />
             </Link>
 
