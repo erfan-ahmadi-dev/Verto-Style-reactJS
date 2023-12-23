7cf8a0d7ac77a9831dc0693c5a59beb530c39471
erfan ahmadi <dornadev@gmail.com>
2023-12-23 13:01:30 +0330

    feat: add to cart slice

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import priceSlice from "./priceAndQuantity/priceSlice";
-import { cartSlice } from "./cart/CartSlice";
+import cartSlice from "./cart/CartSlice";
 
 export const store = configureStore({
   reducer: {
--- src/Redux/cart/CartSlice.js
+++ src/Redux/cart/CartSlice.js
@@ -2,6 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   cart: [],
+  tempCart: [],
 };
 
 export const cartSlice = createSlice({
@@ -9,10 +10,12 @@ export const cartSlice = createSlice({
   initialState,
   reducers: {
     addToCart: (state, action) => {
-      const findIdOfprice = state.items.find(
+      const existingItemIndex = state.cart.findIndex(
         (item) => item.id === action.payload.id
       );
-      if (!findIdOfprice) {
+      if (existingItemIndex !== -1) {
+        state.cart[existingItemIndex].count += action.payload.count;
+      } else {
         state.cart.push(action.payload);
       }
     },
