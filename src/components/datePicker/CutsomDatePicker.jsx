509b5753c8577d761e4fdb2e0b6eda33a21e611f
erfan ahmadi <dornadev@gmail.com>
2023-12-24 15:37:47 +0330

    style : add place holder for Date picker

--- src/components/datePicker/CutsomDatePicker.jsx
+++ src/components/datePicker/CutsomDatePicker.jsx
@@ -1,11 +1,9 @@
-import React, { useEffect, useRef, useState } from "react";
 import DatePicker from "react-multi-date-picker";
 import persian from "react-date-object/calendars/persian";
 import persian_fa from "react-date-object/locales/persian_fa";
-const CustomDatePicker = () => {
-  const [value, setValue] = useState(new Date());
+const CustomDatePicker = ({ dateValue, setDate }) => {
   const handleDateChange = (newValue) => {
-    setValue(newValue);
+    setDate(newValue);
     // Convert the timestamp to a Date object
     const selectedDate = new Date(newValue);
     console.log(selectedDate.getFullYear());
@@ -16,8 +14,10 @@ const CustomDatePicker = () => {
       locale={persian_fa}
       highlightToday="true"
       calendarPosition="bottom-right"
-      value={value}
+      value={dateValue}
       onChange={handleDateChange}
+      inputClass="editInput"
+      placeholder="برای انتخاب تاریخ کلیک کنید"
     />
   );
 };
