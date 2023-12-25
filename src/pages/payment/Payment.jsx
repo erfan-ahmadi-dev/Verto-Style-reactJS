0a4bfa5078cc0b20697a9689ee7b1ef944a80a3b
erfan ahmadi <dornadev@gmail.com>
2023-12-25 16:25:53 +0330

    feat: add payment mock ui

--- src/pages/payment/Payment.jsx
+++ src/pages/payment/Payment.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 function Payment() {
-  return <div></div>;
+  return (
+    <div className="flex items-center w-full justify-center relative font-IranRegular">
+      <img src="paymock.jpg" alt="payment" />
+      <div className="absolute bottom-20 w-5/12 bg-white py-5 right-44 flex justify-around">
+        <button className="px-8 py-2 bg-green-500 text-white">پرداخت</button>
+        <button className="px-8 py-2 bg-red-500 text-white">انصراف</button>
+      </div>
+    </div>
+  );
 }
 
 export default Payment;
