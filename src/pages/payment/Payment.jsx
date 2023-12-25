d0f0daf14cb962ccab6a312ce3b4e720d184fa4c
erfan ahmadi <dornadev@gmail.com>
2023-12-25 17:15:54 +0330

    feat: add redirect to payment page base status

--- src/pages/payment/Payment.jsx
+++ src/pages/payment/Payment.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 
+import { PATHS } from "../../configs/RoutesConfig";
+
 function Payment() {
   return (
     <div className="flex items-center w-full justify-center relative font-IranRegular">
       <img src="paymock.jpg" alt="payment" />
       <div className="absolute bottom-20 w-5/12 bg-white py-5 right-44 flex justify-around">
-        <button className="px-8 py-2 bg-green-500 text-white">پرداخت</button>
-        <button className="px-8 py-2 bg-red-500 text-white">انصراف</button>
+        <button
+          className="px-8 py-2 bg-green-500 text-white"
+          onClick={() => (window.location = `${PATHS.PAYMENT_RESULT}/success`)}
+        >
+          پرداخت
+        </button>
+        <button
+          className="px-8 py-2 bg-red-500 text-white"
+          onClick={() => (window.location = `${PATHS.PAYMENT_RESULT}/failed`)}
+        >
+          انصراف
+        </button>
       </div>
     </div>
   );
